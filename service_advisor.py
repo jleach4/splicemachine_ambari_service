8ac067d6f9cd34082ca3de4c0dfbace45b4f7ba5
John Leach <jleach@splicemachine.com>
2018-01-03 09:12:32 -0800

    Foo

--- service_advisor.py
+++ service_advisor.py
@@ -52,13 +52,35 @@ class HDP26SPLICEMACHINEServiceAdvisor(service_advisor.ServiceAdvisor):
         if property not in core_site or core_site[property] != desired_value:
           putCoreSiteProperty(property, desired_value)
 
+    # Update HBase Classpath
+    if "hbase-env" in services["configurations"]:
+        hbase_env = services["configurations"]["hbase-env"]["properties"]
+        if "content" in hbase_env:
+            content = hbase_env["content"]
+            PXF_PATH = "export HBASE_CLASSPATH=${HBASE_CLASSPATH}:/usr/lib/splicemachine/splicemachine.jar"
+            if "splicemachine.jar" not in content:
+                PXF_PATH = "#Add pxf-hbase.jar to HBASE_CLASSPATH\n" + PXF_PATH
+                content = "\n\n".join((content, PXF_PATH))
+                putHbaseEnvProperty = self.putProperty(configurations, "hbase-env", services)
+                putHbaseEnvProperty("content", content)
+
+
   def getServiceConfigurationsValidationItems(self, configurations, recommendedDefaults, services, hosts):
 
     # validate recommended properties in core-site
     siteName = "core-site"
     method = self.validateCoreSiteConfigurations
     items = self.validateConfigurationsForSite(configurations, recommendedDefaults, services, hosts, siteName, method)
- #   items.extend(resultItems)
+
+    siteName = "hdfs-site"
+    method = self.validateHDFSSiteConfigurations
+    resultItems = self.validateConfigurationsForSite(configurations, recommendedDefaults, services, hosts, siteName, method)
+    items.extend(resultItems)
+
+    siteName = "hbase-site"
+    method = self.validateHBaseSiteConfigurations
+    resultItems = self.validateConfigurationsForSite(configurations, recommendedDefaults, services, hosts, siteName, method)
+    items.extend(resultItems)
 
     return items
 
@@ -72,6 +94,24 @@ class HDP26SPLICEMACHINEServiceAdvisor(service_advisor.ServiceAdvisor):
         validationItems.append({"config-name": property, "item": self.getWarnItem(message)})
     return self.toConfigurationValidationProblems(validationItems, "core-site")
 
+  def validateHDFSSiteConfigurations(self, properties, recommendedDefaults, configurations, services, hosts):
+      hdfs_site = properties
+      validationItems = []
+      for property, desired_value in self.getHDFSSiteDesiredValues().iteritems():
+          if property not in hdfs_site or hdfs_site[property] != desired_value:
+              message = "Splice Machine requires this property to be set to the recommended value of " + desired_value
+              validationItems.append({"config-name": property, "item": self.getWarnItem(message)})
+      return self.toConfigurationValidationProblems(validationItems, "hdfs-site")
+
+  def validateHBaseSiteConfigurations(self, properties, recommendedDefaults, configurations, services, hosts):
+      hbase_site = properties
+      validationItems = []
+      for property, desired_value in self.getHBaseSiteDesiredValues().iteritems():
+          if property not in hbase_site or hbase_site[property] != desired_value:
+              message = "Splice Machine requires this property to be set to the recommended value of " + desired_value
+              validationItems.append({"config-name": property, "item": self.getWarnItem(message)})
+      return self.toConfigurationValidationProblems(validationItems, "hbase-site")
+
   def getCoreSiteDesiredValues(self):
     core_site_desired_values = {
         "ipc.server.listen.queue.size" : "3300"
@@ -80,7 +120,9 @@ class HDP26SPLICEMACHINEServiceAdvisor(service_advisor.ServiceAdvisor):
 
   def getHDFSSiteDesiredValues(self):
     hdfs_site_desired_values = {
-        "dfs.datanode.handler.count" : "20"
+        "dfs.datanode.handler.count" : "20",
+        "dfs.client.read.shortcircuit.buffer.size" : "131072",
+
     }
     return hdfs_site_desired_values
 
@@ -91,6 +133,51 @@ class HDP26SPLICEMACHINEServiceAdvisor(service_advisor.ServiceAdvisor):
         "hbase.regionserver.handler.count" : "200",
         "hbase.client.scanner.caching" : "1000",
         "hbase.hstore.blockingStoreFiles" : "20",
-        "hbase.hstore.compactionThreshold" : "5"
+        "hbase.hstore.compactionThreshold" : "5",
+        "hbase.balancer.period" : "60000",
+        "hbase.client.ipc.pool.size" : "10",
+        "hbase.client.max.perregion.tasks" : "100",
+        "hbase.coprocessor.regionserver.classes" : "com.splicemachine.hbase.RegionServerLifecycleObserver",
+        "hbase.hstore.compaction.max.size" : "260046848",
+        "hbase.hstore.compaction.min.size" : "16777216",
+        "hbase.hstore.compaction.min" : "5",
+        "hbase.hstore.defaultengine.compactionpolicy" : "com.splicemachine.compactions.SpliceDefaultCompactionPolicy",
+        "hbase.hstore.defaultengine.compactor" : "com.splicemachine.compactions.SpliceDefaultCompactor",
+        "hbase.coprocessor.region.classes" : "com.splicemachine.hbase.MemstoreAwareObserver,com.splicemachine.derby.hbase.SpliceIndexObserver,com.splicemachine.derby.hbase.SpliceIndexEndpoint,com.splicemachine.hbase.RegionSizeEndpoint,com.splicemachine.si.data.hbase.coprocessor.TxnLifecycleEndpoint,com.splicemachine.si.data.hbase.coprocessor.SIObserver,com.splicemachine.hbase.BackupEndpointObserver",
+        "hbase.htable.threads.max" : "96",
+        "hbase.ipc.warn.response.size" : "-1",
+        "hbase.ipc.warn.response.time" : "-1",
+        "hbase.master.loadbalance.bytable" : "true",
+        "hbase.htable.threads.max" : "96",
+        "hbase.regions.slop" : "0.01",
+        "hbase.regionserver.global.memstore.size.lower.limit" : "0.9",
+        "hbase.regionserver.lease.period" : "1200000",
+        "hbase.regionserver.maxlogs" : "48",
+        "hbase.regionserver.thread.compaction.large" : "1",
+        "hbase.regionserver.thread.compaction.small" : "4",
+        "hbase.regionserver.wal.enablecompression" : "true",
+        "hbase.rowlock.wait.duration" : "0",
+        "hbase.splitlog.manager.timeout" : "3000",
+        "hbase.status.multicast.port" : "16100",
+        "hbase.wal.disruptor.batch" : "true",
+        "hbase.wal.provider" : "multiwal",
+        "hbase.wal.regiongrouping.numgroups" : "16",
+        "hbase.zookeeper.property.tickTime" : "6000",
+        "hfile.block.bloom.cacheonwrite" : "TRUE",
+        "io.storefile.bloom.error.rate" : "0.005",
+        "splice.authentication.native.algorithm" : "SHA-512",
+        "splice.authentication" : "NATIVE",
+        "splice.client.numConnections" : "1",
+        "splice.client.write.maxDependentWrites" : "60000",
+        "splice.client.write.maxIndependentWrites" : "60000",
+        "splice.compression" : "snappy",
+        "splice.marshal.kryoPoolSize" : "1100",
+        "splice.olap_server.clientWaitTime" : "900000",
+        "splice.ring.bufferSize" : "131072",
+        "splice.splitBlockSize" : "67108864",
+        "splice.timestamp_server.clientWaitTime" : "120000",
+        "splice.txn.activeTxns.cacheSize" : "10240",
+        "splice.txn.completedTxns.concurrency" : "128",
+        "splice.txn.concurrencyLevel" : "4096"
     }
     return hbase_site_desired_values
